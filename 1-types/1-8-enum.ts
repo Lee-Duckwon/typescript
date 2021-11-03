bdc505dbaec2ccee4a2217d56c09df54c4ba706d
Lee-Duckwon <a09519@naver.com>
2021-11-03 23:54:19 +0900

    Edit: enum

--- 1-types/1-8-enum.ts
+++ 1-types/1-8-enum.ts
@@ -31,4 +31,17 @@ import { ENUM } from 'sequelize/types';
   // Monday = 1, 이렇게 정해주면 된다
   // * 문자열도 할당 가능 대신 수동적으로 하나하나 다 할당해야함
   console.log(Days.Tuesday);
+  const day = Days.Monday;
+  //?  타입스크립트에서 enum은 안 쓰는 것이 좋다.
+  //?	day는 const day: Days 이렇게 할 수도 있는데
+  //? enum으로 타입이 지정된 변수에 다른 어떤 숫자도 할당할 수 있다는 것이 문제다
+  //? day: Days = Days.Friday; 를 해도, day = 10; 을 해도 컴파일 에러가 발생하지 않는다
+  //? 즉, enum을 쓰면 타입이 정확하게 보장되지 않는다.
+  //? 상수들을 묶을 수 있는 방법이 enum밖에 없다면 쓸수밖에없지만 타입스크립트에서는 enum보다는
+
+  //! --- union타입을 활용한다. ---//
+  type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday';
+  //* enum을 썼을 때는 보장되지 않으나
+  let dayOfweek: DaysOfWeek = 'Monday'; // * 로 줬을 때
+  dayOfweek = 'Tuesday'; //* 미리 지정한 애들만 줄 수 있다.
 }
