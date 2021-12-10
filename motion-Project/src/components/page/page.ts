d7ba40c0d96e75ee4a4fe00f642e996308625090
Lee-Duckwon <a09519@naver.com>
2021-12-11 02:13:50 +0900

    Edit: Page

--- motion-Project/src/components/page/page.ts
+++ motion-Project/src/components/page/page.ts
@@ -1,6 +1,35 @@
-import { BaseComponent } from './../component.js';
+import { BaseComponent, Component } from './../component.js';
 
-export class PageComponent extends BaseComponent<HTMLUListElement> {
+export interface Composable {
+  // 여러가지 모아서 조립하고 묶을 수 있는
+  addChild(child: Component): void;
+}
+
+class PageItemComponent
+  extends BaseComponent<HTMLElement>
+  implements Composable
+{
+  constructor() {
+    super(`<li class="page-item">
+              <section class="page-item__body"></section>
+              <div class="page-item__controls">
+                <button class="close">&times;</button>
+              </div>
+            </li>`);
+    //
+  }
+  addChild(child: Component) {
+    const container = this.element.querySelector(
+      '.page-item__body'
+    )! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent
+  extends BaseComponent<HTMLUListElement>
+  implements Composable
+{
   // 외부에서 PageComponent 만들어서 필요한 곳에 추가하는 기능
   //private element: HTMLUListElement;
   // element는 dom요소중 하나
@@ -10,7 +39,13 @@ export class PageComponent extends BaseComponent<HTMLUListElement> {
     this.element.setAttribute('class', 'page'); // class 이름 page로 지정
     this.element.textContent = 'This is PageComponent'; // 말그대로 textContent */
 
-    super('<ul class="page">PAgeComponent!</ul>');
+    super('<ul class="page"></ul>');
+  }
+  addChild(section: Component) {
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
+    // 들어오는 섹션이 뭔지 잘 모르지만 무조건 페이지 아이템을 만들어서 전달받은 섹션을 추가해서 받은 다음에 페이지 아이템을 페이지에 넣는다
   }
   /*
   attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
