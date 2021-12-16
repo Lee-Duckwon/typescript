ae14151ad176267fd1a4b5ec2a35187ec7b565e9
Lee-Duckwon <a09519@naver.com>
2021-12-16 23:33:23 +0900

    Edit: app.ts

--- motion-Project/src/app.ts
+++ motion-Project/src/app.ts
@@ -8,18 +8,31 @@ import {
 } from './components/page/page.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { Component } from './components/component.js';
-import { InputDialog } from './components/dialog/dialog.js';
 import { MediaSectionInput } from './components/dialog/input/media-input.js';
 import { TextSectionInput } from './components/dialog/input/text-input.js';
+import {
+  InputDialog,
+  MediaData,
+  TextData
+} from './components/dialog/dialog.js';
+
+type InputComponentConstructor<T = (MediaData | TextData) & Component> = {
+  // * T라는 것은 MediaData 이거나 TextData가 될 것이다.
+  new (): T;
+};
 class App {
   //
   private readonly page: Component & Composable;
 
-  constructor(appRoot: HTMLElement, dialogRoot: HTMLElement) {
+  constructor(appRoot: HTMLElement, private dialogRoot: HTMLElement) {
     this.page = new PageComponent(PageItemComponent);
     this.page.attachTo(appRoot);
     // 페이지 우리가 어디에 붙을거나면 -> appRoot에 너 자신을 추가해줘
-
+    this.bindElementToDialog<MediaSectionInput>(
+      '#new-image',
+      MediaSectionInput,
+      (input: MediaSectionInput) => new ImageComponent(input.title, input.url)
+    );
     const videoBtn = document.querySelector('#new-image')! as HTMLButtonElement;
     videoBtn.addEventListener('click', () => {
       const dialog = new InputDialog();
@@ -69,6 +82,30 @@ class App {
         const image = new TodoComponent(inputSection.title, inputSection.body);
         this.page.addChild(image);
         dialog.removeFrom(dialogRoot);
+      });
+    });
+  }
+  private bindElementToDialog<T extends (MediaData | TextData) & Component>(
+    // dialog 연결해주고 동적으로 요소를 페이지에 추가해주는 것을 함
+    // Element와 dialog를 연결해주는 역할
+    selector: string,
+    InputComponent: InputComponentConstructor<T>,
+    makeSection: (input: T) => Component
+  ) {
+    const element = document.querySelector(selector)! as HTMLButtonElement;
+    element.addEventListener('click', () => {
+      const dialog = new InputDialog();
+      const input = new InputComponent();
+      dialog.addChild(input);
+      dialog.attachTo(this.dialogRoot);
+
+      dialog.setOnCloseListenr(() => {
+        dialog.removeFrom(this.dialogRoot);
+      });
+      dialog.setOnSubmitListenr(() => {
+        const image = makeSection(input);
+        this.page.addChild(image);
+        dialog.removeFrom(this.dialogRoot);
       });
     });
   }
